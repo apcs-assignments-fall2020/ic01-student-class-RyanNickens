443801e6010f8de01d92bb4c2d5efe045314f3fc
RyanNickens <22rnickens@athenian.org>
2020-10-30 17:25:08 +0000

    fully functioning

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,44 @@
 public class Student {
-    
+    public String firstname;
+    public String lastname;
+    public int year;
+
+    public Student(String a, String b, int c) {
+        this.firstname = a;
+        this.lastname = b;
+        this.year = c;
+    }
+
+    public Student() {
+        this.firstname = "Jimmy";
+        this.lastname = "joe";
+        this.year = 2022;
+    }
+
+    public void doSomething() {
+        System.out.println("i am in class");
+    }
+
+    public String toString() {
+        String namename = firstname + " " + lastname;
+        return namename;
+    }
+
+    public String getFirstName() {
+        return this.firstname;
+    }
+
+    public String getLastName() {
+        return this.lastname;
+    }
+    public void setFirstName(String firstname) {
+        if (firstname.trim().length() != 0) {
+            this.firstname = firstname;
+        }
+    }
+    public void setLastName(String firstname) {
+        if (lastname.trim().length() != 0) {
+            this.lastname = lastname;
+        }
+    }
 }
